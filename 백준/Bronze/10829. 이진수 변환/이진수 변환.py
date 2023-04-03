dae602024b64dc92001309592bda4b44797a1037
Kim YeongGwang <92590654+Glory-K@users.noreply.github.com>
2023-04-03 19:59:02 +0900

    [Bronze II] Title: 이진수 변환, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10829. 이진수 변환/이진수 변환.py
+++ 백준/Bronze/10829. 이진수 변환/이진수 변환.py
@@ -1,2 +1,10 @@
-N=input()
-print(bin(int(N))[2:])
+N=int(input())
+res=[]
+def binary(N):
+    if N != 0:
+        res.append(N%2)
+        binary(N//2)
+    return res
+res=binary(N)
+res.reverse()
+print(''.join([str(i) for i in res]))
