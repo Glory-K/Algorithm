f0b14c546c3cb4f269491d83e94d0d09c5b253aa
Kim YeongGwang <92590654+Glory-K@users.noreply.github.com>
2024-01-14 18:12:28 +0900

    [level Lv.1] Title: 나머지가 1이 되는 수 찾기, Time: 0.14 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        while(true){
+            if(n%answer==1){
+                return answer;
+            }
+            answer++;
+        }
+    }
+}
