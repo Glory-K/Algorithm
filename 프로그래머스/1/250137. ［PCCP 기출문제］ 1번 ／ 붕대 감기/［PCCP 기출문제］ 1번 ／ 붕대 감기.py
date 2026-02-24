d83a1eab5ea611b922c3a340ff9b3d5be2531374
Kim YeongGwang <92590654+Glory-K@users.noreply.github.com>
2026-02-24 10:03:14 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.22 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -0,0 +1,34 @@
+def solution(bandage, health, attacks):
+    t, x, y = bandage
+    max_health = health
+    attack_turn = False
+    bandage_cnt = 0
+    i = 0
+    
+    while True:
+        if attacks[0][0] == i:
+            attack = attacks.pop(0)
+            attack_turn = True
+
+        if attack_turn:
+            health -= attack[-1]
+            attack_turn = False
+            
+            if health <= 0:
+                return -1
+            else:
+                bandage_cnt = 0
+                
+        else:
+            health += x
+            bandage_cnt += 1
+            if bandage_cnt == t:
+                bandage_cnt = 0
+                health += y
+            if health > max_health:
+                health = max_health
+        
+        if len(attacks) == 0:
+            return health
+        i+=1
+    return 0
