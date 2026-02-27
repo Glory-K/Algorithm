420157e2e0e0724f8b014192767e6ff61b5c6df9
Kim YeongGwang <92590654+Glory-K@users.noreply.github.com>
2026-02-27 13:41:20 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,13 +1,5 @@
 def solution(a, b):
     month = [31, 29, 31, 30, 31, 30, 31, 31 , 30, 31, 30, 31]
-    
-    date = 0
-    
-    for i in range(0, a-1):
-        date+=month[i]
-    
-    date+=b
-    day = date%7
-    print(date, day)
-    day_eng=["THU","FRI","SAT","SUN","MON","TUE","WED"]
-    return day_eng[day]
+    days = sum(month[:a-1]) + b
+    week = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
+    return week[days % 7]
