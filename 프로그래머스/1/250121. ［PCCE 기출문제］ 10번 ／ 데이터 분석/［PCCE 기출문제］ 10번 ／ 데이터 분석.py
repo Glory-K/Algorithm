0bdacca84e1498f67c5daeaf690095a5ee96765b
Kim YeongGwang <92590654+Glory-K@users.noreply.github.com>
2026-03-05 13:42:08 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.12 ms, Memory: 9.37 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -0,0 +1,17 @@
+def solution(data, ext, val_ext, sort_by):
+    idx = {
+        "code":0,
+        "date":1,
+        "maximum":2,
+        "remain":3
+    }
+    
+    result = []
+    
+    for d in data:
+        if d[idx[ext]] < val_ext:
+            result.append(d)
+            
+    result.sort(key=lambda x: x[idx[sort_by]])
+    
+    return result
